4405b1ebd99e6bbd90d8de0190b479186e3119f9
J-Guenther <jannik.guentheg@gmail.com>
2021-04-04 17:11:56 +0200

    add dialog

--- src/app/actor-add-dialog/actor-add-dialog.component.ts
+++ src/app/actor-add-dialog/actor-add-dialog.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {CharacterService} from "../services/character.service";
+import {ModalController} from "@ionic/angular";
+import {Actor} from "../model/Actor";
+import {Character} from "../model/Character";
 
 @Component({
   selector: 'app-actor-add-dialog',
@@ -7,8 +11,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActorAddDialogComponent implements OnInit {
 
-  constructor() { }
+  addMap = {};
+
+  constructor(public characterService: CharacterService,
+              private modalController: ModalController) { }
 
   ngOnInit() {}
 
+  add(characterId: string) {
+    if (!(characterId in this.addMap)) {
+      this.addMap[characterId] = 1;
+    } else {
+      this.addMap[characterId] += 1;
+    }
+
+  }
+
+  remove(characterId: string) {
+    if (this.addMap[characterId] > 1) {
+      this.addMap[characterId] -= 1;
+    } else {
+      this.addMap[characterId] = 0;
+    }
+  }
+
+  dismiss() {
+    this.modalController.dismiss();
+  }
+
+  addActorsToEncounter() {
+    this.modalController.dismiss({
+      actors: this.getSelectedCharacters()
+    })
+  }
+
+  getSelectedCharacters() {
+    const actors: Actor[] = [];
+    for (const [key, value] of Object.entries(this.addMap)) {
+      if (value > 0) {
+        const character: Character = this.characterService.characters.find(character => character.id === key);
+        for(let i = 0; i < value; i++) {
+          const actor: Actor = {
+            characterId: key,
+            currentHealthPoints: character.healthPoints,
+            majorWound: false,
+            currentAttack: 0,
+            currentSanity: character.sanity,
+            currentMagicPoints: character.magicPoints,
+            currentLuck: character.luck,
+            firearmsReadied: false
+          }
+          actors.push(actor);
+        }
+      }
+    }
+    return actors;
+  }
 }
