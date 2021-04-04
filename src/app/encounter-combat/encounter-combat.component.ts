0fbceb33d25f09a9f5014cdd8dac8efdf1dc4e3c
J-Guenther <jannik.guentheg@gmail.com>
2021-04-04 23:10:31 +0200

    combat order refactoring

--- src/app/encounter-combat/encounter-combat.component.ts
+++ src/app/encounter-combat/encounter-combat.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {Encounter} from "../model/Encounter";
 import {EncounterService} from "../services/encounter.service";
 import {EncounterAddDialogComponent} from "../encounter-add-dialog/encounter-add-dialog.component";
@@ -13,15 +13,13 @@ import {Character} from "../model/Character";
   templateUrl: './encounter-combat.component.html',
   styleUrls: ['./encounter-combat.component.scss'],
 })
-export class EncounterCombatComponent implements OnInit, OnDestroy {
+export class EncounterCombatComponent implements OnInit, OnDestroy, AfterViewInit {
 
   @ViewChild('slides') slides: IonSlides;
 
   encounter: Encounter;
-
-  turn = 0;
-  lastTurn = false;
-  firstTurn = true;
+  isEnd = false;
+  isBeginning = true;
 
   slideOpts = {
     initialSlide: 0,
@@ -34,22 +32,22 @@ export class EncounterCombatComponent implements OnInit, OnDestroy {
     this.encounter = this.encounterService.editEncounter;
   }
 
-  ngOnInit() {
+  async ngOnInit() {
     const deletedCharacterIds: string[] = [];
     for (let i = 0; i < this.encounter.actors.length; i++) {
-       const character: Character = this.characterService.characters.find(
+      const character: Character = this.characterService.characters.find(
         character => character.id === this.encounter.actors[i].characterId
       );
-       if (character) {
-         this.encounter.actors[i].character = character;
-       } else {
-         deletedCharacterIds.push(this.encounter.actors[i].characterId);
-       }
+      if (character) {
+        this.encounter.actors[i].character = character;
+      } else {
+        deletedCharacterIds.push(this.encounter.actors[i].characterId);
+      }
     }
     this.encounter.actors = this.encounter.actors.filter(actor => {
       return !deletedCharacterIds.includes(actor.characterId);
     });
-    this.updateCombatOrder();
+    await this.updateCombatOrder();
   }
 
   async presentModal() {
@@ -73,8 +71,8 @@ export class EncounterCombatComponent implements OnInit, OnDestroy {
     this.encounter.actors.push(...actors);
   }
 
-  public updateCombatOrder(){
-    this.encounter.actors.sort((a,b ) => {
+  public async updateCombatOrder() {
+    this.encounter.actors.sort((a, b) => {
       const dexA = a.firearmsReadied ? a.character.dexterity + 50 : a.character.dexterity;
       const dexB = b.firearmsReadied ? b.character.dexterity + 50 : b.character.dexterity;
       return dexB - dexA;
@@ -83,46 +81,39 @@ export class EncounterCombatComponent implements OnInit, OnDestroy {
     if (this.encounter.currentTurn) {
       const index = this.encounter.actors.indexOf(this.encounter.currentTurn);
       if (index >= 0) {
-        this.turn = index;
-        this.lastTurn = index === this.encounter.actors.length -1;
-        this.slides.update().then(() => this.slides.slideTo(index));
+        await this.slides.update();
+        await this.slides.slideTo(index);
+        this.encounter.currentTurn = this.encounter.actors[index];
       }
     }
-    if (this.turn === 0) {
-      this.firstTurn = true;
-    }
-    this.encounter.currentTurn = this.encounter.actors[this.turn];
+    this.encounter.currentTurn = this.encounter.actors[0];
   }
 
   ngOnDestroy(): void {
     this.encounterService.save(this.encounter);
   }
 
-  nextTurn($event: any) {
-    this.turn += 1;
-    this.encounter.currentTurn = this.encounter.actors[this.turn];
-    this.firstTurn = false;
+  async change($event: any) {
+    const index = await this.slides.getActiveIndex();
+    this.encounter.currentTurn = this.encounter.actors[index];
+    this.isEnd = await this.slides.isEnd();
+    this.isBeginning = await this.slides.isBeginning();
   }
 
-  previousTurn($event: any) {
-    this.turn -= 1;
-    if (this.lastTurn) {
-      this.lastTurn = false;
-    }
-    if (this.turn === 0) {
-      this.firstTurn = true;
-    }
-    this.encounter.currentTurn = this.encounter.actors[this.turn];
-  }
-
-  reachedRoundEnd($event: any) {
-    this.lastTurn = true;
-  }
-
-  newRound(lastTurn: boolean) {
-    if (lastTurn) {
+  async nextTurn() {
+    if (this.isEnd) {
+      await this.slides.slideTo(0);
       this.encounter.currentRound += 1;
-      this.firstTurn = true;
+    } else {
+      await this.slides.slideNext();
     }
+  }
+
+  async prevTurn() {
+    await this.slides.slidePrev();
+  }
+
+  async ngAfterViewInit() {
+    await this.updateCombatOrder();
   }
 }
