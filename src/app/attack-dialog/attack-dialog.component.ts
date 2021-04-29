2d8d2ccd72812d566c6d103a4455cf8ce34c4d73
J-Guenther <jannik.guentheg@gmail.com>
2021-04-29 22:35:54 +0200

    implemented successlevel service

--- src/app/attack-dialog/attack-dialog.component.ts
+++ src/app/attack-dialog/attack-dialog.component.ts
@@ -2,7 +2,9 @@ import {Component, Input, OnInit} from '@angular/core';
 import {ModalController} from "@ionic/angular";
 import {Actor} from "../model/Actor";
 import {Attack} from "../model/Attack";
-import {DiceRoller, RollBase, RootType} from "dice-roller-parser";
+import {DiceRoller} from "dice-roller-parser";
+import {SuccesslevelService} from "../services/successlevel.service";
+import {SuccessLevel} from "../model/SuccessLevel";
 
 @Component({
   selector: 'app-attack-dialog',
@@ -15,7 +17,8 @@ export class AttackDialogComponent implements OnInit {
   @Input() target: Actor;
   @Input() attack: Attack;
 
-  constructor(private modalController: ModalController) { }
+  constructor(private modalController: ModalController,
+              private successlevelService: SuccesslevelService) { }
 
   ngOnInit() {
     console.log(this.target.character.attacks);
@@ -35,8 +38,17 @@ export class AttackDialogComponent implements OnInit {
     // https://www.npmjs.com/package/dice-roller-parser
     const opponentAttack: Attack = $event.detail.value;
     const diceRoller = new DiceRoller();
-    const opponentAttackRoll = diceRoller.roll('1d100');
-    
+    const opponentSuccess = this.successlevelService.calculateSuccessLevel(opponentAttack.skillValue, diceRoller.roll('1d100').value);
+    const mySuccess = this.successlevelService.calculateSuccessLevel(this.attack.skillValue, diceRoller.roll('1d100').value)
+
+    if (mySuccess >= SuccessLevel.FAIL && opponentSuccess >= SuccessLevel.FAIL) {
+      console.log('Both fail');
+    } else if (mySuccess > opponentSuccess || mySuccess === opponentSuccess) {
+      console.log('Attacker wins');
+    } else if (opponentSuccess > mySuccess) {
+      console.log('Defender wins');
+    }
+
     const roll =  diceRoller.roll(opponentAttack.formula.replace('D', 'd'));
     console.log(roll);
   }
